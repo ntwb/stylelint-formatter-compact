9d2331e4f1a75756ebab1fe81c8266d4d5fbe0ae
Stephen Edgar <stephen@netweb.com.au>
2017-05-17 11:32:44 +1000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # stylelint-formatter-compact
 A compact formatter for stylelint
+
+## Installation
+
+```
+npm install stylelint-formatter-compact --save-dev
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+const _ = require("lodash");
+
+const formatter = (results) =>
+    _.flatMap(results, result =>
+        _.map(result.warnings, warning =>
+            `${result.source}: ` +
+            `line ${warning.line}, ` +
+            `col ${warning.column}, ` +
+            `${warning.severity} - ` +
+            `${warning.text}`)).join("\n");
+
+module.exports = formatter;
